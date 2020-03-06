9cff2ae34724ee0179a10336b3be11231a653c03
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2020-03-06 14:24:45 +0900

    Update autosub_octopus.sh

--- autosub_octopus.sh
+++ autosub_octopus.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+subcmd='qsub'
+statcmd='qstat'
+script='md_sigma_add.sh'
 line0='1'
 
 for it in `seq 0 72000`
@@ -9,20 +12,15 @@ tit=`expr $it % 360`
 if [ $tit == 0 ]; then
   echo "################"
   date
-  qstat
+  $statcmd
 fi
 
 line1=`qstat | grep p0.90`
 lc0=${#line0}
 lc1=${#line1}
 if [ $lc0 -gt $lc1 ]; then
-  #echo $line1 
-  #stat1=`echo $line1 | grep RUN` 
-  #statc1=${#stat1}
-  #if [ $lc0 -gt $statc1 ]; then
-    #echo $stat1
     date
-    qsub md_sigma_add.sh
+    $subcmd $script
   #fi
 fi
 
